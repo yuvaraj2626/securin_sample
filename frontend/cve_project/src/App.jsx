6449f3ab126761e8a295ee8b07ca1ad99abc65ac
Yuvaraj R <e0223011@sriher.edu.in>
2026-05-10 20:10:16 +0530

    Fix import case for Navbar componentna

--- frontend/cve_project/src/App.jsx
+++ frontend/cve_project/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import AppRouter from './routers/appRouter.jsx';
 
 const App = () => {
